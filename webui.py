4e4f99d42e60dca7a97a956f4a8f59d3285b9c06
shdenabxl <sjorshnab@gmail.com>
2023-04-10 17:03:00 +0200

    moved graphs to temp folder

--- webui.py
+++ webui.py
@@ -1,5 +1,4 @@
 import multiprocessing
-
 import community
 import community as community_louvain
 import networkx as nx
@@ -33,8 +32,8 @@ def corpus():
     density = nx.density(G)
     partition = community.best_partition(G)
     modularity = community.modularity(partition, G)
-    plot(corp.type_frequency(), filename=f'./temp/corpus-typefreq.html', auto_open=False)
-    plot(corp.plot_histo(), filename=f'./temp/corpus-histo.html', auto_open=False)
+    plot(corp.type_frequency(), filename=f'./static/temp/corpus-typefreq.html', auto_open=False)
+    plot(corp.plot_histo(), filename=f'./static/temp/corpus-histo.html', auto_open=False)
     # degree centrality
     degree_dict = nx.degree_centrality(G)
     degree_df = pd.DataFrame.from_dict(degree_dict, orient='index', columns=['centrality'])
@@ -48,9 +47,9 @@ def corpus():
     fig2 = closeness_df.sort_values('centrality', ascending=False)[0:9].plot.bar(title='Closeness centrality')
     fig3 = betweenness_df.sort_values('centrality', ascending=False)[0:9].plot.bar(title='Betweenness centrality')
     fig1, fig2, fig3 = func.set_axes(fig1), func.set_axes(fig2), func.set_axes(fig3)
-    plot(fig1, filename=f'./temp/corpus-degcent.html', auto_open=False)
-    plot(fig2, filename=f'./temp/corpus-clocent.html', auto_open=False)
-    plot(fig3, filename=f'./temp/corpus-betwcent.html', auto_open=False)
+    plot(fig1, filename=f'./static/temp/corpus-degcent.html', auto_open=False)
+    plot(fig2, filename=f'./static/temp/corpus-clocent.html', auto_open=False)
+    plot(fig3, filename=f'./static/temp/corpus-betwcent.html', auto_open=False)
 
     return render_template('corpus.html', num_artworks=len(corp.artworks), num_saints=len(corp.saints),
                            date=corp.querytime, clustering=clustering, density=density, modularity=modularity)
@@ -62,7 +61,7 @@ def corp_network():
     G2 = Network(width='1450', height='800px', bgcolor='#222222', font_color='white', filter_menu=False)
     G2.from_nx(G)
     G2.force_atlas_2based()
-    G2.save_graph('./temp/corpus-network.html')
+    G2.save_graph('./static/temp/corpus-network.html')
     return render_template('network.html')
 
 
@@ -92,8 +91,8 @@ def saint_page(s):
     G2 = Network(width='100%', height='750', bgcolor='#222222', font_color='white')
     G2.from_nx(G)
     G2.force_atlas_2based()
-    G2.save_graph(f'./temp/{qid}-network.html')
-    plot(s.plot_histogram(), filename=f'./temp/{qid}-histo.html', auto_open=False)
+    G2.save_graph(f'./static/temp/{qid}-network.html')
+    plot(s.plot_histogram(), filename=f'./static/temp/{qid}-histo.html', auto_open=False)
 
     # degree centrality
     degree_dict = nx.degree_centrality(G)
@@ -112,18 +111,18 @@ def saint_page(s):
     fig1, fig2, fig3 = func.set_axes(fig1), func.set_axes(fig2), func.set_axes(fig3)
 
     # plot figures
-    plot(fig1, filename=f'./temp/{qid}-degcent.html', auto_open=False)
-    plot(fig2, filename=f'./temp/{qid}-clocent.html', auto_open=False)
-    plot(fig3, filename=f'./temp/{qid}-betwcent.html', auto_open=False)
+    plot(fig1, filename=f'./static/temp/{qid}-degcent.html', auto_open=False)
+    plot(fig2, filename=f'./static/temp/{qid}-clocent.html', auto_open=False)
+    plot(fig3, filename=f'./static/temp/{qid}-betwcent.html', auto_open=False)
 
     for artwork in objs:
         artworks.append((artwork.QID, artwork.objectvalue, artwork.date, artwork.types))
 
     # plot centrality progression
     centrality_pogression = corp.centrality_evolution(s)
-    plot(centrality_pogression['degree'], filename=f'./temp/{qid}-evolution-degree.html', auto_open=False)
-    plot(centrality_pogression['closeness'], filename=f'./temp/{qid}-evolution-closeness.html', auto_open=False)
-    plot(centrality_pogression['betweenness'], filename=f'./temp/{qid}-evolution-betweenness.html', auto_open=False)
+    plot(centrality_pogression['degree'], filename=f'./static/temp/{qid}-evolution-degree.html', auto_open=False)
+    plot(centrality_pogression['closeness'], filename=f'./static/temp/{qid}-evolution-closeness.html', auto_open=False)
+    plot(centrality_pogression['betweenness'], filename=f'./static/temp/{qid}-evolution-betweenness.html', auto_open=False)
 
     return render_template('saint.html', name=s.name, url=s.url, objs=artworks, qid=qid, clustering=clustering,
                            density=density, modularity=modularity)
@@ -135,11 +134,11 @@ def network_progression(s):
     s = corp.QIDsaints[s]
     ranges = s.network_progression()
     for range in ranges:
-        range['G'].save_graph(f'./temp/{qid}-{range["start"]}-{range["end"]}-network.html')
-        plot(range['degree'], filename=f'./temp/{qid}-{range["start"]}-{range["end"]}-degree.html', auto_open=False)
-        plot(range['closeness'], filename=f'./temp/{qid}-{range["start"]}-{range["end"]}-closeness.html',
+        range['G'].save_graph(f'./static/temp/{qid}-{range["start"]}-{range["end"]}-network.html')
+        plot(range['degree'], filename=f'./static/temp/{qid}-{range["start"]}-{range["end"]}-degree.html', auto_open=False)
+        plot(range['closeness'], filename=f'./static/temp/{qid}-{range["start"]}-{range["end"]}-closeness.html',
              auto_open=False)
-        plot(range['betweenness'], filename=f'./temp/{qid}-{range["start"]}-{range["end"]}-betweenness.html',
+        plot(range['betweenness'], filename=f'./static/temp/{qid}-{range["start"]}-{range["end"]}-betweenness.html',
              auto_open=False)
     return render_template('network_progression.html', ranges=ranges, name=s.name, qid=qid)
 
@@ -173,4 +172,4 @@ def artwork(a):
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
-    app.run(debug=False)
+    app.run(debug=True)
